983db593f2902ebbd57484f83a8af4cdcf3db44d
antonio berardi <adberardi@yahoo.com>
2020-06-04 13:05:06 -0400

    Actividad 3: Agregando envio de mensajes givememsg

--- Actividad_Practica_3/Cliente_Socket.py
+++ Actividad_Practica_3/Cliente_Socket.py
@@ -1,3 +1,5 @@
+#author: Antonio Berardi
+
 import socket
 import sys
 
@@ -6,21 +8,39 @@ conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Indicando dirección IP o dominio a conectarse, y por último el puerto.
 server_address = ("localhost",10601)
-
 #print("------- Nombre puerto {}, número {} ".format(*server_address))
 
 
-def sendHelloIam():
-    conn.send("helloiam usuario_2".encode())
+
+#Función encargada de enviar el mensaje al servidor.
+def sendMsg(message):
     print("------ Enviando mensaje")
+    conn.send(message.encode())
+    msg = ""
     msg = conn.recv(16)
-    print("------- Mensaje recibido:"+format(msg))
+    return msg
+
+#Función encargada de construir el mensaje de autenticación con el servidor.
+def helloIam():
+    result_helloIam = sendMsg("helloiam usuario_2")
+    print("------- Mensaje recibido:"+format(result_helloIam[0:2]))
+
+#Función encargada de construir el mensaje de obtener la longitud del mensaje.
+def msgLen():
+    result_msglen = sendMsg("msglen")
+    print("------- Mensaje recibido:"+format(result_msglen[3:6]))
+
+def giveMeMsg():
+    result_givememsg = sendMsg("givememsg ")
+    print("------- Mensaje recibido:"+format(result_givememsg))
 
 try:
     conn.connect(server_address)
     #socket.create_connection(("localhost",10601))
     print("------- Conectando socket")
-    sendHelloIam()
+    helloIam()
+    msgLen()
+    giveMeMsg()
 except OSError as identifier:
     print("------- Error se presntó:"+identifier)
 finally:
