c021d7dfb0da8872bb0e68117ca826b2a41ad796
antonio berardi <adberardi@yahoo.com>
2020-06-16 20:57:11 -0400

    Iniciando actividad práctica 4 - bloqueos en redis

--- redis_actividad_practica/main.py
+++ redis_actividad_practica/main.py
@@ -1,8 +1,25 @@
-import redis 
+import threading
+import redis
+from redlock import Redlock
 
-r = redis.Redis()
-r.mset({"Venezuela":"Caracas","España":"Madrid","Italia":"Roma"})
 
-print("Insertando en SET.......")
+r = redis.Redis(host="localhost",port=6379,db=0)
+r.set("pais","venezuela")
+redis_server = [{"host":"127.0.0.1","port":6379,"db":0}]
 
-print("Buscando capital de Venezuela: "+format(r.get("Venezuela")))
+
+def main():
+    try:
+        padlock = Redlock(redis_server)
+        l = padlock.lock(r.get("pais"),1000)
+        print("Pais: "+r.get("pais").decode("utf-8"))
+        #Permite conocer el ttl de la variable
+        print(str(l.validity))
+        padlock.unlock(l)
+    except redis.RedisError as identifier:
+        print(identifier)
+
+
+
+t1 = threading.Thread(name="Hilo_1",target=main)
+t1.start()
