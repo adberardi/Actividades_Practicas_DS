e2292b3afd283dc14db3ee598063835dfe58910c
antonio berardi <adberardi@yahoo.com>
2020-06-19 18:51:15 -0400

    Iniciando actividad práctica 4 - Sincronizando parte 2

--- redis_actividad_practica/main.py
+++ redis_actividad_practica/main.py
@@ -1,7 +1,6 @@
 import threading
 import time
 import redis
-import os
 from redlock import Redlock
 
 def acces_resource():
@@ -9,43 +8,47 @@ def acces_resource():
 
 def main():
     try:
-        r = redis.Redis(host="localhost",port=6379,db=0)
-        r.set("pais","venezuela")
-        redis_server = [{"host":"127.0.0.1","port":6379,"db":0}]
-        padlock = Redlock(redis_server)
-        l = padlock.lock(r.get("pais"),5000)
         n = threading.currentThread().getName()
-        #Permite conocer el ttl de la variable
-        #print(str(l.validity))
-
-        if l.validity > 0 and n != "Hilo_2":
-            print("Soy el hilo: "+ n +". Pais: "+r.get("pais").decode("utf-8"))
-        else:
-            time.sleep(5)
-
+        print(str(r.smembers("Hilo")))
+        r.sadd("Hilo",int(n[5]))
+        l = padlock.lock(str(r.smembers("Hilo")),1000)
+        #print(l.resource)
+        #time.sleep(int(n[5]))
+        #r.incrby("pais",int(l.resource.decode("utf-8")))
+        #r.incr("pais")
+        #print(" El primero en llegar fue: "+l.resource.decode("utf-8"))
+        print("Soy: "+ n +". Datos: "+l.resource)
         padlock.unlock(l)
-        
-    except redis.RedisError as identifier:
+
+
+    except Exception as identifier:
         print(identifier)
 
 
 
-
+r = redis.StrictRedis(host="localhost",port=6379,db=0)
+redis_server = [{"host":"127.0.0.1","port":6379,"db":0}]
+padlock = Redlock(redis_server)
+#r.set("pos",1)
+r.sadd("Hilo",0)
+print(str(r.smembers("Hilo")))
+#l = padlock.lock(r.get("pos"),1000)
+# v = int(l.resource.decode("utf-8"))+1
+# l.resource = str(v).encode("utf-8")
 
 t1 = threading.Thread(name="Hilo_1",target=main)
 t2 = threading.Thread(name="Hilo_2",target=main)
 t3 = threading.Thread(name="Hilo_3",target=main)
-# t4 = threading.Thread(name="Hilo_4",target=main)
-# t5 = threading.Thread(name="Hilo_5",target=main)
 
 t1.start()
 t2.start()
 t3.start()
-# t4.start()
-# t5.start()
 
 t1.join()
 t2.join()
 t3.join()
-# t4.join()
-# t5.join()
+
+r.srem("Hilo","0")
+r.srem("Hilo","1")
+r.srem("Hilo","2")
+r.srem("Hilo","3")
