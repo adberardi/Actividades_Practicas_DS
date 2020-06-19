40d883e0a49d04ae0e49ae6ac7a123a493b31147
antonio berardi <adberardi@yahoo.com>
2020-06-18 21:44:54 -0400

    Iniciando actividad práctica 4 - Sincronizando

--- redis_actividad_practica/main.py
+++ redis_actividad_practica/main.py
@@ -13,12 +13,18 @@ def main():
         r.set("pais","venezuela")
         redis_server = [{"host":"127.0.0.1","port":6379,"db":0}]
         padlock = Redlock(redis_server)
-        l = padlock.lock(r.get("pais"),3000)
-        print("Pais: "+r.get("pais").decode("utf-8"))
+        l = padlock.lock(r.get("pais"),5000)
+        n = threading.currentThread().getName()
         #Permite conocer el ttl de la variable
         #print(str(l.validity))
-        
+
+        if l.validity > 0 and n != "Hilo_2":
+            print("Soy el hilo: "+ n +". Pais: "+r.get("pais").decode("utf-8"))
+        else:
+            time.sleep(5)
+
         padlock.unlock(l)
+        
     except redis.RedisError as identifier:
         print(identifier)
 
@@ -29,11 +35,17 @@ def main():
 t1 = threading.Thread(name="Hilo_1",target=main)
 t2 = threading.Thread(name="Hilo_2",target=main)
 t3 = threading.Thread(name="Hilo_3",target=main)
+# t4 = threading.Thread(name="Hilo_4",target=main)
+# t5 = threading.Thread(name="Hilo_5",target=main)
 
 t1.start()
 t2.start()
 t3.start()
+# t4.start()
+# t5.start()
 
 t1.join()
 t2.join()
 t3.join()
+# t4.join()
+# t5.join()
