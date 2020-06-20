688292a67f03f579a4b692bb6864b59dfbee8d20
antonio berardi <adberardi@yahoo.com>
2020-06-20 00:54:50 -0400

    Iniciando actividad práctica 4 - Sincronizando parte 3

--- redis_actividad_practica/main.py
+++ redis_actividad_practica/main.py
@@ -11,9 +11,9 @@ def main():
         n = threading.currentThread().getName()
         print(str(r.smembers("Hilo")))
         r.sadd("Hilo",int(n[5]))
-        l = padlock.lock(str(r.smembers("Hilo")),1000)
+        l = padlock.lock(str(r.smembers("Hilo")),4000)
         #print(l.resource)
-        #time.sleep(int(n[5]))
+        time.sleep(int(n[5]))
         #r.incrby("pais",int(l.resource.decode("utf-8")))
         #r.incr("pais")
         #print(" El primero en llegar fue: "+l.resource.decode("utf-8"))
@@ -31,9 +31,7 @@ redis_server = [{"host":"127.0.0.1","port":6379,"db":0}]
 padlock = Redlock(redis_server)
 #r.set("pos",1)
 r.sadd("Hilo",0)
-print(str(r.smembers("Hilo")))
 #l = padlock.lock(r.get("pos"),1000)
-# v = int(l.resource.decode("utf-8"))+1
 # l.resource = str(v).encode("utf-8")
 
 t1 = threading.Thread(name="Hilo_1",target=main)
