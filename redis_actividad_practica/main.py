a337883c060bcc0c6f812088658a6c5bde8ad8af
antonio berardi <adberardi@yahoo.com>
2020-06-21 02:15:58 -0400

    Iniciando actividad práctica 4 - Acomodando

--- redis_actividad_practica/main.py
+++ redis_actividad_practica/main.py
@@ -1,39 +1,40 @@
 import threading
-import time
 import redis
 from redlock import Redlock
 
-def acces_resource():
-    print("Hello world")
 
 def main():
     try:
+        #Se obtiene el nombre del hilo.
         n = threading.currentThread().getName()
-        print(str(r.smembers("Hilo")))
+        #print(str(r.smembers("Hilo")))
+        #Se actualiza el recurso con el número de Hilo
         r.sadd("Hilo",int(n[5]))
-        l = padlock.lock(str(r.smembers("Hilo")),4000)
-        #print(l.resource)
-        time.sleep(int(n[5]))
-        #r.incrby("pais",int(l.resource.decode("utf-8")))
-        #r.incr("pais")
-        #print(" El primero en llegar fue: "+l.resource.decode("utf-8"))
-        print("Soy: "+ n +". Datos: "+l.resource)
-        padlock.unlock(l)
 
+        #Se bloquea el recurso
+        l = padlock.lock(str(r.smembers("Hilo")),4000)
+
+        #Se imprime el contenido del recurso bloqueado
+        print("Soy: "+ n +". Datos: "+l.resource)
+
+        #Se libera el recurso.
+        padlock.unlock(l)
 
     except Exception as identifier:
         print(identifier)
 
 
+#Se establece la conexión del servidor redis indicando dirección o nombre del host, puerto y base de datos a la que se conectará.
+r = redis.Redis(host="localhost",port=6379)
 
-r = redis.StrictRedis(host="localhost",port=6379,db=0)
-redis_server = [{"host":"127.0.0.1","port":6379,"db":0}]
+#Se establece la dirección y puerto del servidor redis para emplear el manejo de bloqueos.
+redis_server = [{"host":"127.0.0.1","port":6379}]
 padlock = Redlock(redis_server)
-#r.set("pos",1)
-r.sadd("Hilo",0)
-#l = padlock.lock(r.get("pos"),1000)
-# l.resource = str(v).encode("utf-8")
 
+#El recurso a compartir con el resto de los hilos.
+r.sadd("Hilo",0)
+
+#Cada hilo representará una computadora.
 t1 = threading.Thread(name="Hilo_1",target=main)
 t2 = threading.Thread(name="Hilo_2",target=main)
 t3 = threading.Thread(name="Hilo_3",target=main)
@@ -46,6 +47,7 @@ t1.join()
 t2.join()
 t3.join()
 
+#Se elimina los datos almacenados en el par clave-valor Hilo del servidor redis.
 r.srem("Hilo","0")
 r.srem("Hilo","1")
 r.srem("Hilo","2")
