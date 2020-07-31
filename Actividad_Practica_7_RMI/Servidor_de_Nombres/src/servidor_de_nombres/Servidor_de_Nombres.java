85d20e192424bdb7a2f2ee53eb84df5bc4044a5a
adberardi <adberardi@yahoo.com>
2020-07-30 23:11:00 -0400

    Subiendo aplicaciones creadas

---
+++ Actividad_Practica_7_RMI/Servidor_de_Nombres/src/servidor_de_nombres/Servidor_de_Nombres.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidor_de_nombres;
+
+/**
+ *
+ * @author adtony45
+ */
+public class Servidor_de_Nombres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
